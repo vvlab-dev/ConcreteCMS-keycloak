c982c40184058c3c03230fcc3ceecfd0670d720d
Michele Locati <michele@locati.it>
2025-06-03 16:30:40 +0200

    Set minimum core version to 8.5.12 (#8)

--- controller.php
+++ controller.php
@@ -21,7 +21,7 @@ class Controller extends Package implements ProviderAggregateInterface
      *
      * @see \Concrete\Core\Package\Package::$appVersionRequired
      */
-    protected $appVersionRequired = '8.5.10';
+    protected $appVersionRequired = '8.5.12';
 
     /**
      * {@inheritdoc}
